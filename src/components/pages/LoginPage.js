2be79208128e9ef4936c5428a6f854ac4423bb5c
kilam <a2mari.m@gmail.com>
2023-04-17 19:56:16 +0200

    ✨ F01: LOGIN > Add Form and Title

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,8 +1,35 @@
+import { useState } from "react";
+
 export default function LoginPage() {
   // State état ou données
+  const [inputValue, setInputValue] = useState("bob");
 
   // comportements
+  const handelSubmit = (event) => {
+    event.preventDefault();
+    alert(`Bonjour ${inputValue}`);
+    setInputValue("");
+  };
 
+  const handelChange = (event) => {
+    setInputValue(event.target.value);
+  };
   // affichage (render)
-  return <div>LoginPage</div>;
+  return (
+    <div>
+      <h1>Bienvenue chez nous</h1>
+      <br />
+      <h2>Connectez-vous</h2>
+      <form action="submit" onSubmit={handelSubmit}>
+        <input
+          value={inputValue}
+          onChange={handelChange}
+          type="text"
+          placeholder="Entrez votre prenom ..."
+          required
+        />
+        <button>Accédez àvotre espace</button>
+      </form>
+    </div>
+  );
 }
